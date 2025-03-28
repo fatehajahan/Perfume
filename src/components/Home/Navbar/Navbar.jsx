f449967a968bcd57003f38d77b6164b0c33f3500
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-03-28 19:54:57 +0600

    done

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../../assets/homepage/logo1.png'
 import { FaCartArrowDown } from 'react-icons/fa'
 import { FaCircleUser } from 'react-icons/fa6'
@@ -6,7 +6,11 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({ setCartOpen }) => {
   console.log("Navbar received setCartOpen:", typeof setCartOpen);
-  
+  const [profile, setProfile] = useState(false)
+  const profileOpen = () => {
+    setProfile(!profile)
+  }
+
   return (
     <div className='py-[20px] fixed bg-white top-0 left-0 w-full z-50'>
       <div className="container">
@@ -32,7 +36,17 @@ const Navbar = ({ setCartOpen }) => {
                 console.log(setCartOpen, "button")
                 setCartOpen(true)
               }} />
-              <FaCircleUser className='hover:text-[#6a6a6a] transition duration-300' />
+              <FaCircleUser className='hover:text-[#6a6a6a] transition duration-300' onClick={profileOpen} />
+              {
+                profile && (
+                  <div className='absolute right-[250px] top-[90px] z-50'>
+                    <div className='bg-white flex flex-col gap-y-[30px] py-[30px] px-[20px] rounded-lg shadow-lg'>
+                      <Link to="/login" className='bg-black text-white hover:bg-transparent hover:text-black transition duration-500 px-[15px] text-center'>Login</Link>
+                      <Link to="/registration" className='bg-black text-white hover:bg-transparent hover:text-black transition duration-500 px-[15px] text-center'>Registration</Link>
+                    </div>
+                  </div>
+                )
+              }
             </div>
           </div>
         </div>
