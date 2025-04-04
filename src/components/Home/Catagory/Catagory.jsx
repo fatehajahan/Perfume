f8a2c37f6eb825e2106cccd2845e83ad4ef6018f
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-04-04 19:25:45 +0600

    done

--- src/components/Home/Catagory/Catagory.jsx
+++ src/components/Home/Catagory/Catagory.jsx
@@ -16,25 +16,25 @@ const Catagory = () => {
                 </div>
 
                 <div className="catagories md:flex justify-between gap-x-[20px] mt-[40px]">
-                    <div className='md:w-1/3 w-[370px] relative md:mt-0 mt-[40px] md:px-0 px-[20px]'>
+                    <div className='md:w-1/3 relative md:mt-0 mt-[40px] md:px-0 px-[20px]'>
                         <img src={catagory1} alt="" className='w-full' />
-                        <div className='bg-[#ffffff] opacity-[80%] hover:opacity-[100%] transition duration-300 py-[10px] w-[310px] md:w-[400px] absolute bottom-[30px] md:left-[10px] left-[30px] text-center cursor-pointer'>
+                        <div className='bg-[#ffffff] opacity-[80%] hover:opacity-[100%] transition duration-300 py-[10px] md:w-[400px] w-[200px] mx-auto absolute bottom-[30px] left-1/2 transform -translate-x-1/2 text-center cursor-pointer'>
                             <p className='font-cormot text-[16px]  font-bold'>EXCLUSIVE</p>
                             <p className='text-[12px] font-urbanist'>6 Products</p>
                         </div>
                     </div>
 
-                    <div className='md:w-1/3 w-[370px] relative md:mt-0 mt-[40px] md:px-0 px-[20px]'>
+                    <div className='md:w-1/3 relative md:mt-0 mt-[40px] md:px-0 px-[20px]'>
                         <img src={catagory2} alt="" className='w-full' />
-                        <div className='bg-[#fff] opacity-[80%] hover:opacity-[100%] transition duration-300 py-[10px] w-[310px] md:w-[400px] absolute bottom-[30px] md:left-[10px] left-[30px] text-center cursor-pointer'>
+                        <div className='bg-[#fff] opacity-[80%] hover:opacity-[100%] transition duration-300 py-[10px] md:w-[400px] w-[200px] mx-auto absolute bottom-[30px] left-1/2 transform -translate-x-1/2 text-center cursor-pointer'>
                             <p className='font-cormot text-[16px]  font-bold'>MEN</p>
                             <p className='text-[12px] font-urbanist'>6 Products</p>
                         </div>
                     </div>
 
-                    <div className='md:w-1/3 w-[370px] relative md:mt-0 mt-[40px] md:px-0 px-[20px]'>
+                    <div className='md:w-1/3 relative md:mt-0 mt-[40px] md:px-0 px-[20px]'>
                         <img src={catagory3} alt="" className='w-full' />
-                        <div className='bg-[#fff] opacity-[80%] hover:opacity-[100%] transition duration-300 py-[10px] w-[310px] md:w-[400px] absolute bottom-[30px] md:left-[10px] left-[30px] text-center cursor-pointer'>
+                        <div className='bg-[#fff] opacity-[80%] hover:opacity-[100%] transition duration-300 py-[10px] md:w-[400px] w-[200px] mx-auto absolute bottom-[30px] left-1/2 transform -translate-x-1/2 text-center cursor-pointer'>
                             <p className='font-cormot text-[16px]  font-bold'>WOMEN</p>
                             <p className='text-[12px] font-urbanist'>6 Products</p>
                         </div>
