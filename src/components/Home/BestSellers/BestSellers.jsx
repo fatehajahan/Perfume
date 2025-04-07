515c89eeb173d43b65ad29c5c292d0d4cfc5be49
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-04-07 19:01:43 +0600

    done

--- src/components/Home/BestSellers/BestSellers.jsx
+++ src/components/Home/BestSellers/BestSellers.jsx
@@ -14,13 +14,13 @@ const BestSellers = () => {
                     <p className="font-cormot text-base uppercase tracking-wide text-black pt-[50px] text-center font-medium">
                         Check out our
                     </p>
-                    <h3 className='font-cormot text-[68px] text-center py-[20px]'>Best Sellers</h3>
+                    <h3 className='font-cormot md:text-[68px] text-[30px] text-center py-[20px]'>Best Sellers</h3>
                 </div>
 
-                <div className='md:flex justify-between items-center gap-x-[20px] md:mx-0 mx-[20px]'>
-                    <div className='md:my-0 my-[50px]'>
+                <div className='md:flex justify-between items-center gap-x-[20px] mx-auto max-w-[1320px] px-4'>
+                    <div className='md:my-0 my-[30px]'>
                         <Link to="/bluePerfume" className="product1 cursor-pointer">
-                            <img src={product1} alt="" />
+                            <img src={product1} alt="" className='w-[534px] md:w-auto'/>
                             <div className='pt-[15px]'>
                                 <p className='text-[#9D9D9D] text-[15px]'>Exclusive</p>
                                 <p className='font-cormot text-black text-[25px] font-semibold'>Cool Blue Perfume</p>
@@ -41,7 +41,7 @@ const BestSellers = () => {
                     <div className='md:my-0 my-[50px]'>
                         <Link to="/orangePerfume" className="product2 cursor-pointer relative">
                             <div className='absolute top-[30px] left-[30px] bg-white py-[5px] w-[50px] text-center rounded-4xl'><p className='font-urbanist font-bold'>Sale!!</p></div>
-                            <img src={product2} alt="" />
+                            <img src={product2} alt="" className='w-[534px] md:w-auto'/>
                             <div className='pt-[15px]'>
                                 <p className='text-[#9D9D9D] text-[15px]'>Exclusive</p>
                                 <p className='font-cormot text-black text-[25px] font-semibold'>Bright Light by Yana</p>
@@ -60,7 +60,7 @@ const BestSellers = () => {
                     </div>
 
                     <div className="product3 cursor-pointer md:my-0 my-[50px]">
-                        <img src={product3} alt="" />
+                        <img src={product3} alt="" className='w-[534px] md:w-auto'/>
                         <div className='pt-[15px]'>
                             <p className='text-[#9D9D9D] text-[15px]'>Exclusive</p>
                             <p className='font-cormot text-black text-[25px] font-semibold'>Cool Blue Perfume</p>
@@ -83,7 +83,7 @@ const BestSellers = () => {
                     </div>
 
                     <div className="product4 cursor-pointer md:my-0 my-[50px]">
-                        <img src={product4} alt="" />
+                        <img src={product4} alt="" className='w-[534px] md:w-auto'/>
                         <div className='pt-[15px]'>
                             <p className='text-[#9D9D9D] text-[15px]'>Exclusive</p>
                             <p className='font-cormot text-black text-[25px] font-semibold'>Cool Blue Perfume</p>
